a13ec96dd546ecaa0ea3afde45b60eba442e9aa0
MarcioDev <marcio.bodiniguedes@gmail.com>
2024-11-11 21:33:30 -0300

    tabuada do 8

---
+++ tabuada8.py
@@ -0,0 +1,11 @@
+#Tabuada
+def tabuadaDo8():
+    numero = 8
+    print(f"Tabuada do {numero}:")
+    
+    for i in range(1, 11):
+        resultado = numero * i
+        print(f"{numero} x {i} = {resultado}")
+
+
+tabuadaDo8()
