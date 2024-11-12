768054b98d1c8ba475c236ad9c6e4bf2c4e6847d
MarcioDev <marcio.bodiniguedes@gmail.com>
2024-11-11 21:25:13 -0300

    tabuada

--- inicio.py
+++ inicio.py
@@ -1,1 +1,33 @@
-print("Hello, World!")
+#Calc simples em py
+
+
+def somar(num1, num2):
+    return num1 + num2
+
+def subtrair(num1, num2):
+    return num1 - num2
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+def dividir(num1, num2):
+    if num2 == 0:
+        return "Erro: Divisão por zero!"
+    return num1 / num2
+
+def main():
+    print("Bem-vindo à Calculadora!")
+    print("Escolha a operação:")
+    print("1. Soma")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+
+    operacao = input("Digite o número da operação desejada (1/2/3/4): ")
+
+    if operacao in ['1', '2', '3', '4']:
+        numero1 = float(input("Digite o primeiro número: "))
+        numero2 = float(input("Digite o segundo número: "))
+
+        if operacao == '1':
+            resultado = somar(numero1
---
+++ tabuada.py
@@ -0,0 +1,11 @@
+#Tabuada
+def tabuadaDo7():
+    numero = 7
+    print(f"Tabuada do {numero}:")
+    
+    for i in range(1, 11):
+        resultado = numero * i
+        print(f"{numero} x {i} = {resultado}")
+
+
+tabuadaDo7()
