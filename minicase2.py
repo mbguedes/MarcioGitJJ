38a7004498f12665d2b6b633d87bc2bf22b06559
MarcioDev <marcio.bodiniguedes@gmail.com>
2024-11-13 20:43:07 -0300

    minicase 2 e 3

--- minicase2.py
+++ minicase2.py
@@ -1,9 +1,7 @@
 nota1 = int(input("Qual a sua primeira nota: "))
-
 nota2 = int(input("Qual a sua segunda nota: "))
-
 nota3 = int(input("Qual a sua terceira nota: "))
-
 nota4 = int(input("Qual a sua quarta nota: "))
 
-print(((nota1+nota2+nota3+nota4)) / 4)
+media = int(nota1 + nota2 + nota3 + nota4) / 4
+print(f"Caíque, sua média é: {media}")
---
+++ minicase3.py
@@ -0,0 +1,7 @@
+valor = int(input("Por favor, insira o valor a ser trabalhado: "))
+
+print(valor * 2)
+print(valor * 3)
+print(valor ** 2)
+print(valor ** (1/2))
+print(valor ** (1/3))
