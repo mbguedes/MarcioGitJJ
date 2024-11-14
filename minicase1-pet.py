7222bb52eb0c7e26b9812da58f70ad3f7700a167
MBGuedesDev <65191274+mbguedes@users.noreply.github.com>
2024-11-14 19:24:43 -0300

    agora o minicase certo.py

--- minicase1-pet.py
+++ minicase1-pet.py
@@ -4,8 +4,8 @@ def calcularIdadePet ():
     portePet = int(input("Agora me fale o tamanho dele caso seja grande porte digite 1, caso seja médio porte digite 2, e pequeno porte digite 3: "))
     qtdBanho = int(input("E quantas vezes nos últimos 12 meses ele tomou banho conosco? Responda apenas o número de vezes por favor: ")) 
     
-    if portePet == 1: print(input(f"Olá, a idade humana do {nomePet} é {idadePet * 7} e ele já gastou um total de R${qtdBanho*75} conosco"))
-    elif portePet == 2: print(input(f"Olá, a idade humana do {nomePet} é {idadePet * 7} e ele já gastou um total de R${qtdBanho*60} conosco"))
-    elif portePet == 3: print(input(f"Olá, a idade humana do {nomePet} é {idadePet * 7} e ele já gastou um total de R${qtdBanho*50} conosco"))
+    if portePet == 1: print(input(f"Olá, a idade humana do {nomePet} é {idadePet * 7} e nos últimos 12 meses o lucro com este animal foi de R${qtdBanho*55} conosco"))
+    elif portePet == 2: print(input(f"Olá, a idade humana do {nomePet} é {idadePet * 7} e nos últimos 12 meses o lucro com este animal foi de R${qtdBanho*45} conosco"))
+    elif portePet == 3: print(input(f"Olá, a idade humana do {nomePet} é {idadePet * 7} e nos últimos 12 meses o lucro com este animal foi de R${qtdBanho*45}"))
 
 calcularIdadePet()
